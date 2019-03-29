03ca51410d4d81f189dd6bd6f5e8bcb28b82f2dc
Mystique <mystique@quantumfoam.uni.cc>
2019-03-29 23:08:31 +0100

    Updated to work with Response Cards /w DynamoDB
    Updated to work with Response Cards /w DynamoDB

--- valaxy_helpdesk_bot_function.py
+++ valaxy_helpdesk_bot_function.py
@@ -8,17 +8,13 @@ __author__ = 'Mystique'
 .. contactauthor:: miztiik@github issues
 """
 
-######
-# pip3.6 install gdata
-# pip3.6 install --upgrade google-api-python-client
-# pip3.6 install --upgrade google-auth google-auth-oauthlib google-auth-httplib2
-######
-
 import os
 import json
 import logging
 import datetime
 from operator import itemgetter
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
 import asyncio
 
 
@@ -42,6 +38,9 @@ def set_global_vars():
         global_vars['Environment']          = "Test"
         global_vars['tag_key']              = "Valaxy-HelpDesk-Bot"
         global_vars['faq_db_fname']         = "./data/val.json"
+        global_vars['ddb_table_name']       = "valaxy-butler-queries"
+        global_vars['region_name']          = "us-east-1"
+        global_vars['update_ddb']           = True
         global_vars['status']               = True
     except Exception as e:
         logger.error(f"ERROR: {str(e)}")
@@ -62,59 +61,6 @@ def read_from_file(filename):
 
 def safe_div(n, d):
     return n / d if d else 0
-
-
-def slack_msg_builder(msg_data: list):
-    """
-    Parse message in slack attachment format
-
-    :param slack_webhook_url: The lambda event
-    :param type: str
-    :param slack_data: A json containing slack performatted text data
-    :param type: json
-
-    :return: key_exists Returns True, If key exists, False If Not.
-    :rtype: bool
-    """
-    resp = {'status': False}
-    slack_payload = {}
-    slack_payload["text"] = ''
-    slack_payload["attachments"] = []
-    logger.info(msg_data)
-    for i in msg_data:
-        tmp = {}
-        tmp["pretext"]          = f"Check out these demos from #Valaxy Technologies"
-        tmp["fallback"]         = "https://www.youtube.com/c/valaxytechnologies"
-        # tmp["color"]            = i.get("color")        
-        tmp["author_name"]      = "Valaxy Technologies"
-        tmp["author_link"]      = "https://www.youtube.com/c/valaxytechnologies"
-        tmp["author_icon"]      = "https://avatars1.githubusercontent.com/u/12252564?s=400&u=20375d438d970cb22cc4deda79c1f35c3099f760&v=4"
-        tmp["title"]            = f"{i.get('title')}"
-        tmp["title_link"]       = f"https://www.youtube.com/watch?v={i.get('vid_id')}"
-        tmp["fields"]           = [
-                    {
-                        "title": "ViewCount",
-                        "value": f"`{i.get('view_count')}`",
-                        "short": True
-                    },
-                    {
-                        "title": "Popularity",
-                        "value": f"`{i.get('popularity')}`",
-                        "short": True
-                    }
-                ]
-        tmp["thumb_url"]        = i.get('thumbnails')
-        tmp["footer"]           = "Valaxy Butler"
-        tmp["footer_icon"]      = "https://avatars.slack-edge.com/2019-03-25/588415136070_fb5a2f732c948b1c83fe_36.png"
-        tmp["ts"]               = int( datetime.datetime.now().timestamp() )
-        tmp["mrkdwn_in"]        = ["pretext", "text", "fields"]
-        slack_payload["attachments"].append(tmp)
-    # headers={'Content-Type': 'application/json'} 
-    logger.info( json.dumps(slack_payload, indent=4, sort_keys=True) )
-
-    # slack_payload = {'text':json.dumps(i)}
-    return slack_payload
-
 
 #####################################################################################################
 """ --- Helpers to build responses which match the structure of the necessary dialog actions --- """
@@ -152,59 +98,6 @@ def elicit_slot_w_response(session_attributes, intent_name, slots, slot_to_elici
     }
 
 
-def close(session_attributes, fulfillment_state, message):
-    response = {
-        'sessionAttributes': session_attributes,
-        'dialogAction': {
-            'type': 'Close',
-            'fulfillmentState': fulfillment_state,
-            'message': message
-        }
-    }
-
-    return response
-"""
-    "dialogAction": {
-    "type": "Close",
-    "fulfillmentState": "Fulfilled or Failed",
-    "message": {
-      "contentType": "PlainText or SSML or CustomPayload",
-      "content": "Message to convey to the user. For example, Thanks, your pizza has been ordered."
-    },
-   "responseCard": {
-      "version": integer-value,
-      "contentType": "application/vnd.amazonaws.card.generic",
-      "genericAttachments": [
-          {
-             "title":"card-title",
-             "subTitle":"card-sub-title",
-             "imageUrl":"URL of the image to be shown",
-             "attachmentLinkUrl":"URL of the attachment to be associated with the card",
-             "buttons":[ 
-                 {
-                    "text":"button-text",
-                    "value":"Value sent to server on button click"
-                 }
-              ]
-           } 
-       ] 
-     }
-  }
-"""
-def close_w_card(session_attributes, fulfillment_state, message, options):
-    response = {
-        'sessionAttributes': session_attributes,
-        'dialogAction': {
-            'type': 'Close',
-            'fulfillmentState': fulfillment_state,
-            'message': message,
-            "responseCard": build_response_card_slack(options)
-    }
-    }
-    logger.info( json.dumps(response, indent=4, sort_keys=True) )
-    return response
-
-
 def delegate(session_attributes, slots):
     return {
         'sessionAttributes': session_attributes,
@@ -228,52 +121,110 @@ def confirm_intent(session_attributes, intent_name, slots, message, response_car
     }
 
 
-def build_response_card(title, subtitle, options):
+def check_item_exists(region_name: str, table_name: str, needle: str) -> bool:
     """
-    Build a responseCard with a title, subtitle, and an optional set of options which should be displayed as buttons.
+    Check if the given dynamodb item exists.
+    Query with limit of 1 is performed.
     """
-    buttons = None
-    if options is not None:
-        buttons = []
-        for i in range(min(5, len(options))):
-            buttons.append(options[i])
+    if not needle: needle = 'auto'
+    dynamodb = boto3.resource('dynamodb', region_name = region_name)
+    table = dynamodb.Table(table_name)
+    bool_val = False
+    try:
+        response = table.query( KeyConditionExpression=Key('search_query').eq( str(needle).lower() ) )
+        if response.get('Count') == 1 or response.get('Count')>1:
+            bool_val =  True
+    except Exception as e:
+        logger.error(f"ERROR: {str(e)}")
+    return bool_val
 
-    return {
-        'contentType': 'application/vnd.amazonaws.card.generic',
-        'version': 1,
-        'genericAttachments': [{
-            'title': title,
-            'subTitle': subtitle,
-            'buttons': buttons
-        }]
-    }
+
+def create_ddb_item(region_name: str, table_name: str, item: dict):
+    """
+    Create DDB Item
+    """
+    dynamodb = boto3.resource('dynamodb', region_name = region_name)
+    table = dynamodb.Table(table_name)
+    try:
+        response = table.put_item(
+           Item={
+                'search_query': str( item.get('search_query') ),
+                'search_count': 1,
+                'created_on' : str(datetime.datetime.now())
+            }
+        )
+    except Exception as e:
+        logger.error(f"ERROR: {str(e)}")
+
+
+def update_ddb_item(region_name: str, table_name: str, item: dict):
+    """
+    Helper function to Insert / Update item in table
+    Add email_sent attribute and set to true
+    REMOVE next_lead attribute
+    """
+    dynamodb = boto3.resource('dynamodb', region_name = region_name)
+    table = dynamodb.Table(table_name)
+    try:
+        response = table.update_item(TableName = table_name,
+                                        Key={'search_query':str( item.get('search_query') ) },
+                                        # UpdateExpression='SET email_sent= :var1 REMOVE next_lead',
+                                        UpdateExpression='SET search_count= search_count + :incr, last_searched= :var2',
+                                        ExpressionAttributeValues={
+                                                        ':incr' : 1,
+                                                        ':var2' :str(datetime.datetime.now())
+                                                        }
+                                    )
+    except Exception as e:
+        logger.error(f"ERROR: {str(e)}")
 
 
 def build_response_card_slack(options):
     """
     Build a responseCard with a title, subtitle, and an optional set of options which should be displayed as buttons.
     """
-    buttons = None
-    imageUrl = ''
+    cards = []
     if options is not None:
-        buttons = []
-        imageUrl = options[0].get('thumbnails')
-        for i in range(min(5, len(options))):
-            buttons.append( { 'text': options[i].get('title'), 'value': f"https://www.youtube.com/watch?v={options[i].get('vid_id')}"})
 
+        # imageUrl = options[0].get('thumbnails')
+        for i in range(min(5, len(options))):
+            t = {}
+            t['title'] = f"{options[i].get('title')[:75]}..."
+            t['subTitle'] = f"*ViewCount*: *`{options[i].get('view_count')}`* *Popularity*: *`{options[i].get('popularity')}`*"
+            t['attachmentLinkUrl'] = f"https://www.youtube.com/watch?v={options[i].get('vid_id')}"
+            t['imageUrl'] = options[i].get('thumbnails')
+            cards.append(t)
     return {
         'contentType': 'application/vnd.amazonaws.card.generic',
         'version': 1,
-        'genericAttachments': [{
-            'title': 'Have a look at these demonstrations',
-            'subTitle': 'What will you build today?',
-            'attachmentLinkUrl': 'https://www.youtube.com/c/valaxytechnologies',
-            'imageUrl': imageUrl,
-            'buttons': buttons
-        }]
+        'genericAttachments': cards
+        }
+
+
+def close_w_card(session_attributes, fulfillment_state, message, options):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message,
+            "responseCard": build_response_card_slack(options)
+        }
     }
+    logger.info( json.dumps(response, indent=4, sort_keys=True) )
+    return response
 
 
+def close(session_attributes, fulfillment_state, message_content):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message_content
+        }
+    }
+    return response
 #####################################################################################################
 """ --- End of Helper Functions --- """
 #####################################################################################################
@@ -283,15 +234,28 @@ def build_response_card_slack(options):
 def get_video_id_intent(global_vars: dict, intent_request: dict) -> dict:    
     resp = { "status": False, "error_message": "", 'id_lst': [] }
 
-    haystack = read_from_file( global_vars.get('faq_db_fname') )
-
     slots = intent_request['currentIntent']['slots']
+    logger.info(type(slots))
+    logger.info(slots)
     output_session_attributes = intent_request['sessionAttributes'] if intent_request['sessionAttributes'] is not None else {}
     # If no slots were matched return
     if not slots['slot_one_svc']:
-        resp['error_message'] = f"Your query does not match any AWS Service found. Please try again"
-        return resp
+        return elicit_slot(output_session_attributes, 
+                            intent_request['currentIntent']['name'],
+                            slots,
+                            'slot_one_svc',
+                            f"Your query does not match any AWS Service found. Please try again"
+                            )
+    # Insert / Update Dynamodb about search query
+    item = { 'search_query': slots['slot_one_svc'].lower() }
+    if not check_item_exists( global_vars.get('region_name'), global_vars.get('ddb_table_name'), slots['slot_one_svc'] ):
+        create_ddb_item(global_vars.get('region_name'), global_vars.get('ddb_table_name'), item)
+    else:
+        update_ddb_item(global_vars.get('region_name'), global_vars.get('ddb_table_name'), item)
+    
+    # Begin searching for the search_query(needle) in the haystack
     v_ids = []
+    haystack = read_from_file( global_vars.get('faq_db_fname') )
     for i in haystack['vids']:
         if slots['slot_one_svc'].lower() in i[0]['title'].lower():
             # resp['id_lst'].append( {'title': i[0]['title'] , 'id': i[0]['vid_id'] } )
@@ -319,48 +283,21 @@ def get_video_id_intent(global_vars: dict, intent_request: dict) -> dict:
             Popularity: ( LikeCount / TotalFeedback )
         """
         filter_1 = 10
-        filter_2 = 3
+        filter_2 = 5
         s1_v_ids = sorted(v_ids, key=itemgetter('view_count'), reverse=True)[:filter_1]
         s2_v_ids = sorted(s1_v_ids, key=itemgetter('popularity'), reverse=True)[:filter_2]
 
-        # Prepare message for slack
-        # msg = slack_msg_builder(s2_v_ids)
-
         #resp['id_lst'] = s2_v_ids
         resp['status'] = True
-    # return resp
-    """
-    return elicit_slot(
-                output_session_attributes,
-                intent_request['currentIntent']['name'],
-                slots,
-                validation_result['violatedSlot'],
-                validation_result['message'],
-                build_response_card(
-                    'Specify {}'.format(validation_result['violatedSlot']),
-                    validation_result['message']['content'],
-                    build_options(validation_result['violatedSlot'], appointment_type, date, booking_map)
-                )
-            )
-    return close(
-        output_session_attributes,
-        'Fulfilled',
-        {
-            'contentType': 'PlainText',
-            'content': json.dumps( resp['id_lst'] )
-        }
-    )
-    """
+
+
     return close_w_card(
         output_session_attributes,
         'Fulfilled',
-        {
-            'contentType': 'PlainText',
-            'content': json.dumps( s2_v_ids )
-        },
+        {'contentType': 'PlainText', 'content': 'Have a look at these demonstrations from #Valaxy'},
         s2_v_ids
     )
-    
+
 
 def dispatch(global_vars, intent_request):
     """
@@ -433,16 +370,11 @@ def lambda_handler(event, context):
 
     resp_chk(global_vars.get('status'), global_vars.get('error_message'))
 
-    faq_db = read_from_file( global_vars.get('faq_db_fname') )
+    # faq_db = read_from_file( global_vars.get('faq_db_fname') )
 
-    # event['currentIntent']['name']
     needle = None
     if 'currentIntent' in event and 'slots' in event['currentIntent']:
-        needle = event['currentIntent']['slots']
-        
-    # v_ids = get_video_id_intent(needle, faq_db)
-    
-    # resp['v_ids'] = v_ids.get('id_lst')
+        needle = event['currentIntent']['slots']       
 
     logger.debug(f"event.bot.name={event['bot']['name']}")
 
